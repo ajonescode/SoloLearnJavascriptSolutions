c9b563e41ae4821af838d0ad3f74950902fb4b3b
ajonescode <54915239+ajonescode@users.noreply.github.com>
2021-08-15 17:04:29 -0500

    Add files via upload

---
+++ 2.2 SoloLearn_Your first Project.js
@@ -0,0 +1,3 @@
+//print "JS is fun"
+
+console.log("JS is fun")
---
+++ 4.2 Even Numbers_Comments.js
@@ -0,0 +1,13 @@
+// Comments
+
+
+// The program you’re given outputs a list of names to the console.
+
+// Your mission, should you choose to accept it, is to change the code to output only the items with even numbers.
+
+//console.log("1. James")
+console.log("2. John")
+//console.log("3. Amy")
+console.log("4. Collin")
+//console.log("5. Tom")
+console.log("6. Lisa")
